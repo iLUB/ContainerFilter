8b8dee15fbdfdf3bb799f6a1f110c429011c27a8
Fabio Heer <fabio.heer@ilub.unibe.ch>
2014-12-23 19:19:30 +0100

    Transfer Responsibility

--- plugin.php
+++ plugin.php
@@ -12,5 +12,5 @@ $ilias_min_version = "4.2.0";
 $ilias_max_version = "4.3.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
-$responsible = "Fabio Heer";
-$responsible_mail = "fabio.heer@ilub.unibe.ch";
+$responsible = "Timon Amstutz - University of Bern";
+$responsible_mail = "timon.amstutz@ilub.unibe.ch";
