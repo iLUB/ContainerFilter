157323c835df8c6a52b4dcd8e24757384a35c486
Tomasz <thomas.kolonko@ilub.unibe.ch>
2015-08-13 09:27:57 +0200

    Reversed prior commit due to error.

--- templates/jquery.livesearch.js
+++ templates/jquery.livesearch.js
@@ -88,10 +88,10 @@
 
 		displayResults:function (scores) {
 			var self = this;
-			this.list.find(this.line).parent().hide();
+			this.list.find(this.line).hide();
 			this.list.find('.ilPDBlockSubHeader').hide();
 			$.each(scores, function (i, score) {
-				self.rows[score.index].parent().show();
+				self.rows[score.index].show();
 
 				var $subitems = self.rows[score.index].find('.subitem');
                 $subitems.hide();
